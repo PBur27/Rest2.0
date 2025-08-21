b6f965a9d4e4ed1b31dc34a496bb298371037d09
PBur27 <p.burda2003@gmail.com>
2025-08-22 00:39:33 +0200

    start

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1}}>
+      <HomeScreen />
+    </View>
+  );
+}
---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View style={{ flex: 1, flexDirection: "column" }}>
+      <View
+        style={{
+          backgroundColor: "dimgray",
+          flex: 1,
+        }}
+      ></View>
+      <View
+        style={{
+          backgroundColor: "offwhite",
+          flex: 8,
+        }}
+      ></View>
+      <View
+        style={{
+          backgroundColor: "green",
+          flex: 1,
+        }}
+      ></View>
+      <View
+        style={{
+          backgroundColor: "dimgray",
+          flex: 2,
+        }}
+      ></View>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
